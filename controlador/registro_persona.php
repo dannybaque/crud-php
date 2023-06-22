68977b81ff7473ab82a4dd7db645ffad2ad50ea4
dannybaque <baquedanny9@gmail.com>
2023-06-22 02:09:59 -0500

    escribir este si es

--- controlador/registro_persona.php
+++ controlador/registro_persona.php
@@ -1,21 +1,28 @@
 <?php
-    if (!empty($_POST[btnregistrar])) {
-        if (!empty($_POST["nombre"]) and !empty($_POST["apellido"]) and !empty($_POST["cedula"]) and !empty($_POST["fechanacimiento"]) and !empty($_POST["edad"])) {
-            $nombre=$_POST["nombre"];
-            $apellido=$_POST["apellido"];
-            $cedula=$_POST["cedula"];
-            $fechanacimiento=$_POST["fechanacimiento"];
-            $edad=$_POST["edad"];
+    if (!empty($_POST['btnregistrar'])) {
+        if (!empty($_POST['nombre']) and 
+        !empty($_POST['apellido']) and 
+        !empty($_POST['cedula']) and 
+        !empty($_POST['fechanacimiento']) and 
+        !empty($_POST['edad'])) {
+            
+            $nombre=$_POST['nombre'];
+            $apellido=$_POST['apellido'];
+            $cedula=$_POST['cedula'];
+            $fechanacimiento=$_POST['fechanacimiento'];
+            $edad=$_POST['edad'];
+            
 
-            $sql=$conexion->query("insert into persona (nombre,apellido,cedula,fecha_nacimiento,edad)values('$nombre','$apellido','$cedula','$fechanacimiento','$edad')" );
+            $sql = $conexion->query("INSERT INTO persona (nombre, apellido, cedula, fecha_nacimiento, edad) VALUES ('$nombre', '$apellido', '$cedula', '$fechanacimiento', '$edad')");
+
             if ($sql==1) {
-                echo '<div class="alert alert-success">Persona registrado correctamente</div>';
+                echo '<div>Persona registrado correctamente</div>';
             }else {
-                echo '<div class="alert alert-danger">Error al registrar persona</div>';
+                echo '<div>Error al registrar persona</div>';
             }
-
         }else{
-            echo '<div class="alert alert-warning">Error al registrar persona</div>';
+            echo '<div>Error al registrar persona</div>';
         }
     }
 ?>
+
