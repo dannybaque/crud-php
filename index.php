d2637192cbdeaacc3a08529ee3a06c1b589f9a37
dannybaque <baquedanny9@gmail.com>
2023-06-22 00:24:23 -0500

    leer

--- index.php
+++ index.php
@@ -43,18 +43,22 @@
       <th scope="col">Apellido</th>
       <th scope="col">Cedula</th>
       <th scope="col">Fecha de nacimiento</th>
-      <th scope="col">Cedula</th>
+      <th scope="col">Edad</th>
       <th scope="col"></th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+    <?php
+        include "modelo/conexion.php";
+        $sql=$conexion->query("select * from persona");
+        while($datos=$sql->fetch_object()){?>
+            <tr>
+      <td><?= $datos->id ?></td>
+      <td><?= $datos->nombre ?></td>
+      <td><?= $datos->apellido ?></td>
+      <td><?= $datos->cedula ?></td>
+      <td><?= $datos->fecha_nacimiento ?></td>
+      <td><?= $datos->edad ?></td>
       <td>
         <a href="" class="btn btn-small btn-warning">
         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit" width="32" height="32" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -74,6 +78,9 @@
 </svg></a>
       </td>
     </tr>
+        <?php }
+        ?>
+    
   </tbody>
 </table>
     </div>
